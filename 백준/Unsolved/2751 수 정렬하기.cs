f4d4ceebe5ed8f55fa41564350a2fa0ed5137283
LKP <wssmash93@gmail.com>
2022-03-16 19:20:54 +0900

    1차 시간초과

--- 백준/Unsolved/2751 수 정렬하기.cs
+++ 백준/Unsolved/2751 수 정렬하기.cs
@@ -9,32 +9,47 @@ namespace _2751
         static void Main(string[] args)
         {
             StreamReader sr = new StreamReader(Console.OpenStandardInput());
-            List<string> list = new List<string>();
+            List<int> list = new List<int>();
             int N = int.Parse(sr.ReadLine());
             for (int i = 0; i < N; i++)
-                list.Add(sr.ReadLine());
+                list.Add(int.Parse(sr.ReadLine()));
 
+            QuickSort(list, 0, N - 1);
 
-            for (int i = 0; i < N; i++)
-                Console.WriteLine(string.Join("\n",list));
+            Console.WriteLine(string.Join("\n", list));
         }
-        static void QuickSort(List<string> list, int left, int right)
+        static void QuickSort(List<int> list, int start, int end)
         {
-            if(left<right)
+            if (start < end)
             {
+                int pivot = Partition(list, start, end);
+
+                QuickSort(list, start, pivot - 1);
+                QuickSort(list, pivot, end);
             }
         }
-        static int Partition(List<string> list, int left, int right)
+        static int Partition(List<int> list, int start, int end)
         {
-            int pivot = list[left];
+            int pivot = list[start];
 
-            while(left<right)
+            while (start <= end)
             {
-                while(pivot < list[right])
-                    right--;
-                while(pivot > list[left])
-                    left++;
+                while (list[end] > pivot)
+                    end--;
+                while (list[start] < pivot)
+                    start++;
+
+                if (start <= end)
+                    Swap(list, start++, end--);
+                    
             }
+            return start;
+        }
+        static void Swap(List<int> list, int idx1, int idx2)
+        {
+            int tmp = list[idx1];
+            list[idx1] = list[idx2];
+            list[idx2] = tmp;
         }
     }
 }
