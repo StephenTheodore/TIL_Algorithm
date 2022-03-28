b1c9668775981ef6409eb3a766bba69fd39667e2
Stephen Theodore <wssmash93@gmail.com>
2022-03-29 00:34:43 +0900

    채점 대기

--- 백준/Unsolved/12865 심상치않은 배낭.cs
+++ 백준/Unsolved/12865 심상치않은 배낭.cs
@@ -6,34 +6,40 @@ namespace _12865_심상치않은배낭
 {
     class Program
     {
-        static int[] dp = new int[2];
+        static int[,] dp;
+        static int N, K, max = 0;
+        static int[] W, V;
         static void Main(string[] args)
         {
             StreamReader sr = new StreamReader(Console.OpenStandardInput());
             string[] input1 = sr.ReadLine().Split(' ');
-            int N = input1[0];
-            int M = input1[1];
-
             string[] input2;
-            int W = 0;
-            int V = 0;
-            for (int i = 0; i < N; i++)
+            N = int.Parse(input1[0]) + 1;
+            K = int.Parse(input1[1]) + 1;
+            W = new int[N];
+            V = new int[N];
+            dp = new int[N, K];
+
+            for (int i = 1; i < dp.GetLength(0); i++)
             {
                 input2 = sr.ReadLine().Split(' ');
-                W = input2[0];
-                V = input2[1];
+                W[i] = int.Parse(input2[0]);
+                V[i] = int.Parse(input2[1]);
+                PutIn(i, W[i], V[i]);
             }
+            Console.WriteLine(max);
         }
-        static int[] DFS(int weight,int value)
+        static void PutIn(int amount, int weight, int value)
         {
-            /*
-            dp[Weight][Value]
-            물건의 갯수는 영향을 받지 않음
-            Weight는 K를 넘을수 없음
-
-            무게와 가치를 Memoization할 것
-            각 무게에 대해 DFS, 최대 Value를 반환
-            */
+            for (int i = 0; i < dp.GetLength(1); i++)
+            {
+                if (i >= weight)
+                    dp[amount, i] = Math.Max(dp[amount - 1, i], value + dp[amount - 1, i - weight]);
+                else
+                    dp[amount, i] = dp[amount - 1, i];
+                if (max < dp[amount, i])
+                    max = dp[amount, i];
+            }
         }
     }
 }
