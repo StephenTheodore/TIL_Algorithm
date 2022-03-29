6ff08d24eee11e570c76e544b22d18ae79c100c1
Stephen Theodore <wssmash93@gmail.com>
2022-03-29 18:47:30 +0900

    백준허브 자동업로드로 인한 기존파일 삭제

--- 프로그래머스/C#/Solved Old/Level1/문자열을 정수로 바꾸기.cs
+++
@@ -1,7 +0,0 @@
-public class Solution {
-    public int solution(string s) {
-        int answer = 0;
-        answer = int.Parse(s);
-        return answer;
-    }
-}
--- 프로그래머스/C#/Solved Old/Level1/평균 구하기.cs
+++
@@ -1,8 +0,0 @@
-public class Solution {
-    public double solution(int[] arr) {
-        double answer = 0;
-        foreach(int val in arr)
-            answer += val;
-        return answer/arr.Length;
-    }
-}
