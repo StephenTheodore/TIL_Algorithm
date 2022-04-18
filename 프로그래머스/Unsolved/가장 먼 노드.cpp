72488377e9d762cbd2fdacc67d24a247d73a5279
Stephen Theodore <wssmash93@gmail.com>
2022-04-19 00:34:27 +0900

    푸는 중

--- 프로그래머스/Unsolved/가장 먼 노드.cpp
+++ 프로그래머스/Unsolved/가장 먼 노드.cpp
@@ -1,51 +1,63 @@
 #include <iostream>
 #include <vector>
-#include <queue>
-#include <string>
+#include <deque>
 #include <map>
 
 using namespace std;
 
-int solution(int n, vector<vector<int>> edges) {
+int solution(int n, vector<vector<int>> edges)
+{
+    int max = 0;
     int answer = 0;
     map<int, int> nodeMap;
-    queue<int> curNode;
-
-    curNode.push(1);
-    nodeMap.insert(1, 0);
-    while (!edges.empty())
+    deque<int> curNode, nextNode;
+    
+    curNode.push_back(1);
+    nodeMap.emplace(1, 0);
+    for (int i = 0; !edges.empty(); i++)
     {
-        queue<int> nextNode;
-        for (int i = 0; i < curNode.size(); i++)
+        for (const int &node : curNode)
         {
-            int node = curNode.front();
-            curNode.pop();
-            for (vector<vector<int>>::iterator iter = edges.begin(); iter != edges.end();)
+            if (nodeMap.find(node) != nodeMap.end())
+                continue;
+            for (vector<vector<int>>::iterator edge = edges.begin(); edge != edges.end();)
             {
-                vector<int> edge = *iter;
-                //새 노드가 발견되면 edges로부터 순차적으로 지우지 말고 한번에 지우기
-                if (edge[0] == node)
+                int start = edge->front();
+                int dest = edge->back();
+                if (start == node)
                 {
-                    nodeMap.insert(edge[0], i + 1);
-                    iter = edges.erase(iter);
+                    if (nodeMap.find(dest) == nodeMap.end())
+                    {
+                        nextNode.push_back(dest);
+                        nodeMap.emplace(dest, i + 1);
+                    }
+                    edge = edges.erase(edge);
                 }
-                else if (edge[1] == node)
+                else if (dest == node)
                 {
-                    nodeMap.insert(edge[1], i + 1);
-                    iter = edges.erase(iter);
+                    nextNode.push_back(start);
+                    edge++;
                 }
                 else
-                    iter++;
+                    edge++;
             }
         }
         curNode = nextNode;
+        nextNode.clear();
     }
-    //아직 방문하지 않은 노드는 Map에 없다.
-    //지워진 값들은 현재 Map의 지나온 간선개수 +1을 하여 저장한다. 이때 기존값이 있으면 패스
-
+    for (map<int,int>::iterator dist = nodeMap.begin(); dist != nodeMap.end();)
+    {
+        if (max < dist->second)
+        {
+            max = dist->second;
+            answer = 0;
+        }
+        answer++;
+        dist++;
+    }
     return answer;
 }
 int main()
 {
-
+    cout << solution(6, { {3,6},{4,3},{3,2},{1,3},{1,2},{2,4},{5,2} }) << endl;
 }
