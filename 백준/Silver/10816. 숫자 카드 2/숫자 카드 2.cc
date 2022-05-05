3bd62c6fe59249cd61b6e07610aac2c8a75190e6
김영진 <84778417+StephenTheodore@users.noreply.github.com>
2022-05-05 23:26:13 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 392 ms, Memory: 79300 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int N, M, value;
+	const int Size = 10000000;
+	vector<int> AP(Size), AN(Size);
+
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++)
+	{
+		scanf("%d", &value);
+		if (value >= 0)
+			AP[value]++;
+		else
+			AN[value * -1]++;
+	}
+	scanf("%d", &M);
+	for (int i = 0; i < M; i++)
+	{
+		scanf("%d", &value);
+		printf("%d ", value >= 0 ? AP[value] : AN[value * -1]);
+	}
+	printf("\n");
+}
