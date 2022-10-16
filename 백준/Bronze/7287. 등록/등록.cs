681e4768ac00d6b58c857eded6509d0f19015a57
김영진 <84778417+StephenTheodore@users.noreply.github.com>
2022-10-16 17:26:19 +0900

    [Bronze V] Title: 등록, Time: 48 ms, Memory: 5320 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cs
@@ -0,0 +1,12 @@
+using System.Text;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        StringBuilder sb = new();
+        sb.AppendLine("124");
+        sb.AppendLine("wssmash93");
+        Console.Write(sb.ToString());
+    }
+}
