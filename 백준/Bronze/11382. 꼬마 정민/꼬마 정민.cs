0aff371a788442beb905c9847d43518f201bca5d
김영진 <84778417+StephenTheodore@users.noreply.github.com>
2022-10-16 17:54:43 +0900

    [Bronze V] Title: 꼬마 정민, Time: 64 ms, Memory: 5496 KB -BaekjoonHub

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cs
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cs
@@ -3,6 +3,9 @@ static class Program
     static void Main(string[] args)
     {
         StreamReader sr = new(Console.OpenStandardInput());
-        Console.WriteLine(Array.ConvertAll(sr.ReadLine().Split(' '), Convert.ToInt64).ToList().Sum());
+        long sum = 0;
+        foreach (string str in sr.ReadLine().Split(' '))
+            sum += Convert.ToInt64(str);
+        Console.WriteLine(sum);
     }
 }
