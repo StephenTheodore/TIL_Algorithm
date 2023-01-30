626c4d40278017dcf92262604740c0e27bda6f9e
김영진 <84778417+StephenTheodore@users.noreply.github.com>
2023-01-30 21:56:47 +0900

    [level 0] Title: 암호 해독, Time: 0.32 ms, Memory: 30.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Text;
+
+public class Solution {
+    public string solution(string cipher, int code) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = code-1; i<cipher.Length; i+=code)
+            sb.Append(cipher[i]);
+        
+        return sb.ToString();
+    }
+}
